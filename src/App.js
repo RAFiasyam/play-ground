aaf7168862a637253d2ec6ec26db19db863d5ff6
RAFiasyam <rafi.asyam23@gmail.com>
2024-11-15 23:53:05 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './Counter';
+import Items from './Items';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Items />
     </div>
   );
 }
---
+++ src/Items.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function Items() {
+    const [items, setItems] = useState([]);
+
+    const removeItem = (index) => {
+        const newItems = items.filter((_, i) => i !== index);
+        setItems(newItems);
+    }
+
+    const addItem = (newItem) => {
+        setItems([...items, newItem]);
+    }
+
+    return (
+        <div>
+            <button onClick={() => addItem('New items')}>Add a item</button>
+            {items.length > 0 && (
+                items.map((item, index) => (
+                    <div key={index}>
+                        {item}
+                        <button onClick={() => removeItem(index)}>Remove</button>
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
+
+export default Items
